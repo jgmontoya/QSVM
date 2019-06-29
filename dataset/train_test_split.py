2e1de8ce78584047f906f1827238c9ec7325a278
jgmontoya <jgmontoya@uc.cl>
2019-06-29 12:31:29 +0900

    Add train/test dataset split

---
+++ dataset/train_test_split.py
@@ -0,0 +1,13 @@
+# Split the dataset in training set and testing set in order to be consistent on the comparisons
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('Iris.csv', index_col=0)
+
+SEED = 42
+
+# 80/20 holdout split
+train, test = train_test_split(df, stratify=df['Species'], test_size=30, random_state=SEED)
+
+train.to_csv('Iris_training.csv')
+test.to_csv('Iris_testing.csv')
